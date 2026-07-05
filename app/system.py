d757cad4eb3cc85e4c749de597959ab2f25dc440
gc1644 <gleb@duck.com>
2026-07-05 16:58:32 +0300

    spaces

--- app/system.py
+++ app/system.py
@@ -5,23 +5,29 @@ import psutil
 def get_hostname():
     return platform.node()
 
+
 def get_os():
     return platform.system()
+
 
 def get_kernel():
     return platform.release()
 
+
 def get_cpu_usage():
     return psutil.cpu_percent(interval=1)
+
 
 def get_memory_usage():
     return psutil.virtual_memory().percent
 
+
 def get_disk_usage():
     return psutil.disk_usage("/").percent
 
+
 def get_system_info():
-    return { 
+    return {
         "system": {
             "hostname": get_hostname(),
             "os": get_os(),
